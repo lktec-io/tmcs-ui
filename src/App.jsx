b69ae03374164259bcc7ad76acdbb3098d531422
lktec-io <lktecio@gmail.com>
2025-11-18 13:03:22 +0300

    -------

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Register from './component/Register';
-import AllStudents from './component/AllStudents';
+import Register from './components/Register';
+import AllStudents from './components/AllStudents';
 
 export default function App() {
   return (
